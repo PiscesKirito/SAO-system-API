b05f4bbb9c6100c2fe500df99ff791826331c395
PiscesKirito <850627623@qq.com>
2020-12-31 20:06:48 +0800

    {Gin} Initial project construction

---
+++ SAO-gin/Route/route.go
@@ -0,0 +1,22 @@
+package route
+
+import (
+	"net/http"
+	"sao/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+// InitRoute 初始化路由
+func InitRoute() {
+	route := gin.Default()
+	// 要在路由组之前全局使用「跨域中间件」, 否则OPTIONS会返回404
+	route.Use(middleware.Cors())
+	normal := route.Group("normal")
+	{
+		normal.GET("/carousel", func(c *gin.Context) {
+			c.String(http.StatusOK, "HelloWorld")
+		})
+	}
+	route.Run(":8082")
+}
---
+++ SAO-gin/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"sao/route"
+)
+
+func main() {
+	route.InitRoute()
+}
